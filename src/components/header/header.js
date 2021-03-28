b627ddf610e97f4bb9f0ed32c634d93491c49af4
Ron Belkin <ronbel249@gmail.com>
2021-03-28 14:23:13 +0300

    Added mobile drawer

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './header.module.scss';
-
+import {
+    BrowserView,
+    MobileView,
+} from "react-device-detect";
+import MobileDrawer from '../mobile-drawer/mobile-drawer';
 
 export default function Header() {
 
@@ -9,10 +13,13 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <h1>Weather App</h1>
-            <nav className={styles['links-container']}>
+            <BrowserView viewClassName={styles['links-container']}>
                 <NavLink exact className={styles.link} activeClassName={styles.active} to="/">Home</NavLink>
                 <NavLink exact className={styles.link} activeClassName={styles.active} to="/favorites">Favorites</NavLink>
-            </nav>
+            </BrowserView>
+            <MobileView>
+                <MobileDrawer/>
+            </MobileView>
         </header>
     )
 }
---
+++ src/components/mobile-drawer/mobile-drawer.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Drawer, Button } from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
+import styles from './mobile-drawer.module.scss';
+import { NavLink as Link } from 'react-router-dom';
+
+export default function MobileDrawer() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openDrawer = () => setIsOpen(true);
+    const closeDrawer = () => setIsOpen(false);
+
+    return (
+        <>
+            <Button onClick={openDrawer}>
+                <Menu fontSize="large" style={{ color: 'white' }} />
+            </Button>
+
+            <Drawer anchor="right" onClose={closeDrawer} open={isOpen}>
+                <div className={styles['drawer-content']}>
+                    <Link onClick={closeDrawer} exact activeClassName={styles.active} className={styles.link} to="/">Home</Link>
+                    <Link onClick={closeDrawer} exact activeClassName={styles.active} className={styles.link} to="/favorites">Favorites</Link>
+                </div>
+            </Drawer>
+        </>)
+
+
+}
